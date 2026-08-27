61d5e5cf05bdded2bda15cb017e4babaea2f7677
Julian Starks <julianstarks218@gmail.com>
2026-08-27 15:37:42 +0000

    Added README.MD

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# CS121_project_1
+# CS121_project_
+
+My name is **Julian**
+
+I am excited to learn about:
+- C#
+* C+
++ JavaScript
---
+++ project1/hello.py
@@ -0,0 +1,7 @@
+userName = input("Hello! What is your name? ").lower()
+if userName == "julian":
+    print(f"Hey {userName}! You created me!")
+else:
+     print(f"Hey {userName}! Lovely weather isn't it?")
+
+
