5fb95ae0075600fd30252c65829bfa31b24e9cd2
Julian Starks <julianstarks218@gmail.com>
2026-08-28 16:18:44 +0000

    Updated hello.py

--- project1/hello.py
+++ project1/hello.py
@@ -1,7 +1,9 @@
 userName = input("Hello! What is your name? ").lower()
 if userName == "julian":
     print(f"Hey {userName}! You created me!")
+elif userName == "julien":
+    print(f">:(")
 else:
-     print(f"Hey {userName}! Lovely weather isn't it?")
+    print(f"Hey {userName}! Lovely weather isn't it?")
 
 
